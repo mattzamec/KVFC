b6200586caf329b1ecbdb9cc59060df2b7e12e40
Matt Zamec <matt.zamec@gmail.com>
2016-11-12 16:45:37 -0800

    Aiveo #25 - default prder cost type to 'fixed' instead of an empty value

--- shop/food/edit_member_types.php
+++ shop/food/edit_member_types.php
@@ -3,21 +3,18 @@ include_once 'config_openfood.php';
 session_start();
 valid_auth('site_admin,member_admin');
 
-
-if (!$_GET['content']) $_GET['content'] = 'members';
+if (!$_GET['content'])
+{
+    $_GET['content'] = 'members';
+}
 
 // Get the membership_types array and prepare the membership_types table update form (in case needed)
-$query = '
-  SELECT
-    *
-  FROM
-    '.TABLE_MEMBERSHIP_TYPES;
-$sql = @mysql_query($query, $connection) or die("Couldn't execute query 6.");
-$membership_types_array = array ();
-$membership_type_ids = array ();
+$sql = @mysql_query('SELECT * FROM '.TABLE_MEMBERSHIP_TYPES, $connection) or die("Couldn't execute query 6.");
+$membership_types_array = [];
+$membership_type_ids = [];
 
 while ( $row = mysql_fetch_object($sql) )
-  {
+{
     $membership_types_array[$row->membership_type_id]['initial_cost'] = $row->initial_cost;
     $membership_types_array[$row->membership_type_id]['order_cost'] = $row->order_cost;
     $membership_types_array[$row->membership_type_id]['order_cost_type'] = $row->order_cost_type;
@@ -30,19 +27,19 @@ while ( $row = mysql_fetch_object($sql) )
     array_push ($membership_type_ids, $row->membership_type_id);
 
     if ($row->order_cost_type == 'percent')
-      {
+    {
         $order_cost_display = number_format ($row->initial_cost, 2).'&nbsp;%';
         $form_order_cost_unit = '';
         $cost_type_fixed = '';
         $cost_type_percent = ' selected';
-      }
-    elseif ($row->order_cost_type == 'fixed')
-      {
+    }
+    else     // if order cost type is not explicitly set to percent, default it to fixed
+    {
         $order_cost_display = '$&nbsp;'.number_format ($row->initial_cost, 2);
         $form_order_cost_unit = '$&nbsp;';
         $cost_type_fixed = ' selected';
         $cost_type_percent = '';
-      }
+    }
 
     $form_cost_type_display = '
       <select name="order_cost_type['.$row->membership_type_id.']">
@@ -76,10 +73,10 @@ while ( $row = mysql_fetch_object($sql) )
         </fieldset>
       </div>
       ';
-  }
+}
 
 if ($_GET['content'] == 'membership_types')
-  {
+{
     // Send the membership_types display and form
     $page_specific_javascript .= '
       <script type="text/javascript">
@@ -122,72 +119,55 @@ if ($_GET['content'] == 'membership_types')
         '.$membership_types_display.'
         <input id="form_submit" type="submit" name="action" value="Update ALL Membership Type Changes" style="display:none;">
       </form>';
-  }
-
-
-
-
-
+}
 
 // If not using jquery and doing an update, then get the posted info and do it...
 if ($_GET['content'] == 'members' && $_POST['update'] == 'Make changes')
-  {
+{
     foreach (array_keys ($_POST['membership_type_id']) as $member_id)
-      {
+    {
         if ($_POST['membership_type_id'][$member_id] != $_POST['old_membership_type_id'][$member_id])
-          {
+        {
             $query = '
-              UPDATE
-                '.TABLE_MEMBER.'
-              SET
-                membership_type_id = "'.mysql_real_escape_string ($_POST['membership_type_id'][$member_id]).'"
-              WHERE
-                member_id = "'.$member_id.'"';
+              UPDATE '.TABLE_MEMBER.'
+              SET membership_type_id = "'.mysql_real_escape_string ($_POST['membership_type_id'][$member_id]).'"
+              WHERE member_id = "'.$member_id.'"';
             $result= mysql_query($query) or die("Error: " . mysql_error());
-          }
-      }
-  }
+        }
+    }
+}
 
 // Display the member table and current values with forms for jquery (ajax) or manual (post) update...
 if ($_GET['content'] == 'members')
-  {
-    if ($_GET['page'] > 1)
-      {
-        $page = floor($_GET['page']);
-      }
-    else
-      {
-        $page = 1;
-      }
+{
+    $page = ($_GET['page'] > 1 ? floor($_GET['page']) : 1);
     $sort = $_GET['sort'];
     // If the sort is not a valid value, default it to "member_id"
     if (strpos ('|member_id|business_name|last_name|username|membership_type_id|', "|$sort|") === false)
-      {
+    {
         $sort = 'member_id';
-      }
+    }
     $sort_by[$sort] = '&nbsp;(&#8593;)';
     $query = '
-      SELECT
-        COUNT(member_id) AS number_of_members
-      FROM
-        '.TABLE_MEMBER;
+      SELECT COUNT(member_id) AS number_of_members
+      FROM '.TABLE_MEMBER;
     $sql = @mysql_query($query, $connection) or die("Couldn't execute query 6.");
     $row = mysql_fetch_object($sql);
     $number_of_pages = ceil ($row->number_of_members / PER_PAGE);
 
     for ( $page_number = 1; $page_number <= $number_of_pages; $page_number ++ )
-      {
+    {
         if ($page_number == $page)
-          {
+        {
             $page_links .= '
               <span class="this_page">'.($page_number).'</span> ';
-          }
+        }
         else
-          {
+        {
             $page_links .= '
               <a class="page_link" href="'.$_SERVER['SCRIPT_NAME'].'?content=members&page='.$page_number.'&sort='.$sort.'">'.$page_number.'</a> ';
-            }
-      }
+        }
+    }
 
     $member_form .= '
       <th><a href="'.$_SERVER['SCRIPT_NAME'].'?content=members&page='.$page.'&sort=member_id">ID'.$sort_by['member_id'].'</a></th>
@@ -207,30 +187,27 @@ if ($_GET['content'] == 'members')
         preferred_name,
         auth_type,
         membership_type_id
-      FROM
-        '.TABLE_MEMBER.'
-      ORDER BY
-        '.mysql_real_escape_string ($sort).'
-      LIMIT
-        '.(($page - 1) * PER_PAGE).', '.PER_PAGE;
+      FROM '.TABLE_MEMBER.'
+      ORDER BY '.mysql_real_escape_string ($sort).'
+      LIMIT '.(($page - 1) * PER_PAGE).', '.PER_PAGE;
     $sql = @mysql_query($query, $connection) or die("Couldn't execute query 6.");
 
     while ( $row = mysql_fetch_object($sql) )
-      {
+    {
         $class_pending = '';
         $pending_statement = '';
         if ($row->pending == '1')
-          {
+        {
             $class_pending = ' pending';
             $pending_statement = '<br>[PENDING]';
-          }
+        }
         $class_producer = '';
         $producer_statement = '';
         if (in_array ('producer', explode (',', $row->auth_type)))
-          {
+        {
             $class_producer = ' producer';
             $producer_statement = '[PRODUCER] ';
-          }
+        }
         $first_name = $row->first_name;
         $last_name = $row->last_name;
         $first_name_2 = $row->first_name_2;
@@ -244,32 +221,32 @@ if ($_GET['content'] == 'members')
             <td class="username'.$class_pending.$class_producer.'">'.$row->username.$pending_statement.'</td>
             <td id="row['.$row->member_id.']" class="radio'.$class_pending.$class_producer.'">';
         if ($_SESSION['member_id'] == $row->member_id)
-          {
+        {
             $member_form .= '
               SELF: Modification is not permitted';
-          }
+        }
         else
-          {
+        {
             $member_form .= '
               <select name="membership_type_id['.$row->member_id.']" onchange="update_member_info(\''.$row->member_id.'\',\'membership_type_id\',this.options[this.selectedIndex].value);">';
             foreach ($membership_type_ids as $membership_type_id)
-              {
+            {
                 $type_check = '';
                 if ($row->membership_type_id == $membership_type_id)
-                  {
+                {
                     $type_check = ' selected';
-                  }
+                }
                 $member_form .= '
             <option'.$type_check.' value="'.$membership_type_id.'">'.$membership_types_array[$membership_type_id]['membership_class'].'</option>';
-              }
+            }
             $member_form .= '
               </select>
               <input type="hidden" name="old_membership_type_id['.$row->member_id.']" value="'.$row->membership_type_id.'">';
-          }
+        }
         $member_form .= '
             </td>
           </tr>';
-      }
+    }
 
     $page_specific_css = '
       <style type="text/css">
@@ -330,13 +307,12 @@ if ($_GET['content'] == 'members')
       <div id="submit_button"><input type="submit" name= "update" value="Make changes">
       <input type="reset" name= "reset" value="Reset values"></div>
       </form>';
-  }
+}
 
 $page_title_html = '<span class="title">Membership Information</span>';
 $page_subtitle_html = '<span class="subtitle">Modify Membership Types</span>';
 $page_title = 'Membership Information: Modify Membership Types';
 $page_tab = 'member_admin_panel';
-
 
 include("template_header.php");
 echo '
@@ -344,4 +320,3 @@ echo '
   '.$content_types.'
   <!-- CONTENT ENDS HERE -->';
 include("template_footer.php");
-
